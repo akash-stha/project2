81c6b980a36939747c6f3280efac3f3512275d56
Akash Shrestha <aakashtha49@gmail.com>
2023-12-08 10:05:19 -0500

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Akash Shrestha on 2023-12-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
