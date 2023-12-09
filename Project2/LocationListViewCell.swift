2ac717cbbf77c209c546bdf4398205fc38f1413e
Akash Shrestha <aakashtha49@gmail.com>
2023-12-08 19:33:25 -0500

    Until callouts done

---
+++ Project2/LocationListViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationListViewCell.swift
+//  Project2
+//
+//  Created by Akash Shrestha on 2023-12-08.
+//
+
+import UIKit
+
+class LocationListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Project2/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Project2
+//
+//  Created by Akash Shrestha on 2023-12-08.
+//
+
+import Foundation
